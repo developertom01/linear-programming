8d8e2050beba70f59a85ef9bbf027ddcd343a03b
Thomas <thomas.sarpong@yum.com>
2023-10-19 17:48:00 -0400

    fix

--- script.py
+++ script.py
@@ -1,5 +1,6 @@
 from scipy.optimize import linprog
 
+
 """"
 Get coefficients of inequalities Given that:
  maximize -19 * x + -15 * y 
@@ -10,27 +11,31 @@ Get coefficients of inequalities Given that:
          -16 * x + 18 * y <= 50 
          -3 * x + 3 * y <= 80
 """
-# Coefficients of linear function
-c = [-19, -15]
+# Coefficients of the negative linear objective function (to maximize, we minimize the negative)
+c = [19, 15]
 
-# Given that A_ub @ x <= b_ub
-
-# A_ub - Coefficients of inequalities [[x,y]]
+# Coefficients of the inequality constraints: A_ub @ x <= b_ub
 A_ub = [[-2, -10], [-16, 18], [-3, 3]]
-# b_ab : Y intercepts of the inequalities [1,2,4]
 b_ub = [40, 50, 80]
 
-# Also Given that A_eq @ x == b_eq -
 # Bounds for variables
 x_bounds = (-10, 10)
-y_bound = (-10, 10)
-bounds = [x_bounds, y_bound]
-optimal = linprog(c, A_ub=A_ub, b_ub=b_ub, bounds=bounds, method="highs")
+y_bounds = (-10, 10)
+
+# Solve the linear programming problem
+optimal = linprog(c, A_ub=A_ub, b_ub=b_ub, bounds=[x_bounds, y_bounds], method="highs")
 
 if optimal.success:
-    print(optimal.message)
-    print("optimal objective function value ", optimal.fun ,)
-    print("alues of the decision variables that minimizes the objective function while satisfying the constraints. ", optimal.x)
-
+    # Print the optimal values and the actual objective function value (negative of what was minimized)
+    print("Optimal values of x and y:", optimal.x[0], optimal.x[1])
+    print("Maximized objective function value:", -optimal.fun)  # Negate to get the actual maximum value
 else:
-    print("Iteration terminated with status", optimal.status, optimal.message)
+    print("Iteration terminated with status", optimal.status)
+
+
+"""
+The answers expected  are 
+Optimal values of x and y: -6.224489795918367 -2.7551020408163267
+Maximized objective function value: 159.59183673469389
+
+"""
